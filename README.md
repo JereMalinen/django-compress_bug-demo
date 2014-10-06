11e139f27adcaa19d69235bfcf348d69a192dfbd
Jere Malinen <jere.malinen@arcusys.fi>
2014-10-06 15:13:32 +0300

    Add project files

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 django-compress_bug-demo
 ========================
 
-Demo about django-compress bug
+To run the project:
+
+cd django-compress_bug-demo
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
+cd compresstest/
+./manage.py runserver 8001
+
+
+Try setting COMPRESS_ENABLED = False, and see that the text is red.
---
+++ compresstest/compresstest/settings.py
@@ -0,0 +1,97 @@
+"""
+Django settings for compresstest project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '30fp%b&l3i_*x&w6zz#zlzuxqo@rp#ph$&yz&79+6&np-l!1vh'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'compressor',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'compresstest.urls'
+
+WSGI_APPLICATION = 'compresstest.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'fi'
+
+TIME_ZONE = 'Europe/Helsinki'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+COMPRESS_ENABLED = True
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'compresstest/', 'static/')
+
+STATICFILES_DIRS = (COMPRESS_ROOT,)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'compresstest/', 'templates/'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'compressor.finders.CompressorFinder',
+)
